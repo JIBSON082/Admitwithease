91c144e604be23237bdec71fd367f7275786af88
JIBSON082 <105949776+JIBSON082@users.noreply.github.com>
2026-06-06 17:57:29 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,11 @@
 
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  integrations: [tailwind()],
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
+});
